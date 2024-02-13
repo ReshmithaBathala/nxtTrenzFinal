f12d77e2897bbbf1711cbd3983b0cc3eb048241b
ReshmithaBathala <reshmithabathala26@gmail.com>
2024-02-13 16:14:30 +0000

    NxtTrenz

---
+++ src/components/CartItem/index.js
@@ -0,0 +1,84 @@
+import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const CartItem = props => (
+  <CartContext.Consumer>
+    {value => {
+      const {
+        removeCartItem,
+        incrementCartItemQuantity,
+        decrementCartItemQuantity,
+      } = value
+      const {cartItemDetails} = props
+      const {title, brand, quantity, price, imageUrl, id} = cartItemDetails
+
+      const onDeleteCartItem = () => {
+        removeCartItem(id)
+      }
+
+      const incrementCount = () => {
+        incrementCartItemQuantity(id)
+      }
+
+      const decrementCount = () => {
+        decrementCartItemQuantity(id)
+      }
+
+      return (
+        <li className="cart-item">
+          <img className="cart-product-image" src={imageUrl} alt={title} />
+          <div className="cart-item-details-container">
+            <div className="cart-product-title-brand-container">
+              <p className="cart-product-title">{title}</p>
+              <p className="cart-product-brand">by {brand}</p>
+            </div>
+            <div className="cart-quantity-container">
+              <button
+                type="button"
+                className="quantity-controller-button"
+                data-testid="minus"
+                aria-label="DashSquare"
+                onClick={decrementCount}
+              >
+                <BsDashSquare color="#52606D" size={12} />
+              </button>
+              <p className="cart-quantity">{quantity}</p>
+              <button
+                type="button"
+                className="quantity-controller-button"
+                aria-label="Quantity"
+                data-testid="plus"
+                onClick={incrementCount}
+              >
+                <BsPlusSquare color="#52606D" size={12} />
+              </button>
+            </div>
+            <div className="total-price-delete-container">
+              <p className="cart-total-price">Rs {price * quantity}/-</p>
+              <button
+                className="remove-button"
+                type="button"
+                onClick={onDeleteCartItem}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+          <button
+            className="delete-button"
+            type="button"
+            aria-label="Circle"
+            onClick={onDeleteCartItem}
+          >
+            <AiFillCloseCircle color="#616E7C" size={20} />
+          </button>
+        </li>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartItem
